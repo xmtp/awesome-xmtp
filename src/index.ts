abcbb2773e471bfa1b3f23f0245c359a09ea440f
fabri <fguespe@gmail.com>
2024-04-14 00:54:13 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -29,34 +29,45 @@ run(async (context: HandlerContext) => {
     "3": "AI Docs Bot docs.hi.xmtp.eth : Chat with the XMTP Docs through an API with a GPT powered bot. Powered by Kapa.",
     "4": "GeneralStore Bot store.hi.xmtp.eth : Bot for ordering goodies in hackathons.",
     "5": "Wordle a Day Bot dailywordle.eth : Play daily through XMTP to the WORDLE game created by du8.",
-    "6": "Starter Bot: 0x3E4EFc2B2Ee3fCE01433F2E75021eeACd62CA94f : A bot that simply echoes what you send it.",
+    "6": "Starter Bot: echo.hi.xmtp.eth : A bot that simply echoes what you send it.",
+    "7": "Awesome Bot: awesome.hi.xmtp.eth : Learn evetything about frames and bots.",
   };
+
+  const framesInfo = {
+    "1": "Farguessr Frame: Guess the distance between 2 countries",
+    "2": "Openframedl Frame: Wordle in a frame",
+    "3": "Zora Magic Machine Frame: Zora newsletter",
+    "4": "Rock Paper Scissors Frame: Rock paper scissors game",
+    "5": "FC Polls: Farcaster Polls working on messaging apps",
+    "6": "Mint Frame: Frame for redirecting users to mint on Zora",
+    "7": "Transaction Frame: Frame that acts a transaction receipt",
+    "8": "Gitcoin Grants: Grant that showcases Gitcoin grants",
+  };
+  const frameUrls = [
+    "Farguessr Frame: https://farguessr.vercel.app/",
+    "Openframedl Frame: https://openframedl.vercel.app/",
+    "Zora Magic Machine Frame: https://paragraph.xyz/@zora/zora-magic-machine/",
+    "Rock Paper Scissors Frame: https://xmtp-frame-rock-paper-scissors.vercel.app/",
+    "FC Polls: https://fc-polls.vercel.app/",
+    "Mint Frame: https://trending-mints.vercel.app/?chain=base&a=0x87c082a2e681f4d2da35883a1464954d59c35d3a&c=790",
+    "Transaction Frame: https://tx-receipt.vercel.app/api/image?networkId=holesky",
+  ];
+
   if (!step) {
-    // send the first message
     const fullBotDescriptions = Object.entries(botInfo)
-      .map(([key, value]) => `${key}. ${value}`)
-      .join("\n\n");
+      .map(([, value]) => `- ${value}`)
+      .join("\n");
     await context.reply(
-      `Welcome to the Awesome XMTP Bot. Explore the world of XMTP bots and frames! 🤖🖼️`
+      `Welcome to the Awesome XMTP Bot. Explore bots and frames! 🤖🖼️`
     );
-    await context.reply(
-      `Bots:\n\n${fullBotDescriptions}\n\nCheck out the repositories in Awesome XMTP https://github.com/xmtp/awesome-xmtp and give the repo a star 🤩`
-    );
+    await context.reply(`Bots:\n\n${fullBotDescriptions}`);
 
+    const framesMessage =
+      `Also, explore these Frames compatible with Open Frames 🖼️:\n\n` +
+      frameUrls.map((url) => `- ${url}`).join("\n") +
+      `\n\nDiscover more frames in Awesome Open Frames https://github.com/open-frames/awesome-open-frames ✨.`;
+
+    await context.reply(framesMessage);
     inMemoryCache.set(senderAddress, 1);
-  } else if (step === 1) {
-    //@ts-ignore
-    const selectedBotInfo = botInfo[content];
-    if (!selectedBotInfo) {
-      await context.reply(
-        "Invalid option selected. Please enter a valid option (1 through 9)"
-      );
-      return;
-    }
-
-    await context.reply(selectedBotInfo);
-
-    // Optionally, move to the next step or reset the step for future interactions
-    inMemoryCache.delete(senderAddress); // Reset the step for future interactions
   }
 });
